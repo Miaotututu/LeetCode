d6b1a5de0dc50007d51bea92e1958b54908990b4
Miaotututu <237378656@qq.com>
2020-09-28 22:55:07 +0800

    Initial commit

---
+++ src/easy/Question13.java
@@ -0,0 +1,43 @@
+package easy;
+
+public class Question13 {
+    /**
+     * 按照题目的描述，可以总结如下规则：
+     *
+     * 罗马数字由 I,V,X,L,C,D,M 构成；
+     * 当小值在大值的左边，则减小值，如 IV=5-1=4；
+     * 当小值在大值的右边，则加小值，如 VI=5+1=6；
+     * 由上可知，右值永远为正，因此最后一位必然为正。
+     * 一言蔽之，把一个小值放在大值的左边，就是做减法，否则为加法。
+     *
+     */
+    public int romanToInt(String s) {
+        int sum = 0;
+        int preNum = getValue(s.charAt(0));
+        for(int i = 1;i < s.length(); i ++) {
+            int num = getValue(s.charAt(i));
+            if(preNum < num) {
+                sum -= preNum;
+            } else {
+                sum += preNum;
+            }
+            preNum = num;
+        }
+        sum += preNum;
+        return sum;
+    }
+
+    private int getValue(char ch) {
+        switch(ch) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+
+}
